a2209f1f597376977c1e07bc5f8517bfc1eeb42f
Simon Wiippola <simon.wiippola@devcode.se>
2017-02-04 10:18:14 +0100

    Added CompetenceController

---
+++ src/CeViche_Backend/Controllers/CompetenceController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using CeViche_Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CeViche_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class CompetenceController : Controller
+    {
+        // GET api/competence
+        [HttpGet]
+        public IEnumerable<Competence> GetAllCompetences()
+        {
+            //Not implemented
+            return null;
+        }
+
+        // GET api/competence/5
+        [HttpGet("{id}")]
+        public Competence GetCompetence(int id)
+        {
+            //Not implemented
+            return null;
+        }
+
+        // POST api/competence
+        [HttpPost]
+        public void AddNewCompetence(Competence competence)
+        {
+        }
+
+        // PUT api/competence/5
+        [HttpPut("{id}")]
+        public void UpdateCompetence(int id, Competence value)
+        {
+        }
+
+        // DELETE api/competence/5
+        [HttpDelete("{id}")]
+        public void DeleteCompetence(int id)
+        {
+        }
+    }
+}
