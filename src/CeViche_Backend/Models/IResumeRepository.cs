51b4cc80fed333d179e4d478fb8801d1f3453762
Gustaf Liljegren <gustafl@bahnhof.se>
2017-02-04 10:51:43 +0100

    Added ResumeRespository.

---
+++ src/CeViche_Backend/Models/IResumeRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeViche_Backend.Models
+{
+    public interface IResumeRepository
+    {
+        void Add(Resume item);
+        IEnumerable<Resume> GetAll();
+        Resume Find(string key);
+        Resume Remove(string key);
+        void Update(Resume resume);
+    }
+}
---
+++ src/CeViche_Backend/Models/ResumeRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeViche_Backend.Models
+{
+    public class ResumeRepository : IResumeRepository
+    {
+        private static ConcurrentDictionary<string, Resume> _resumes = new ConcurrentDictionary<string, Resume>();
+
+        public ResumeRepository()
+        {
+
+        }
+
+        public void Add(Resume resume)
+        {
+            resume.Key = Guid.NewGuid().ToString();
+            _resumes[resume.Key] = resume;
+        }
+
+        public IEnumerable<Resume> GetAll()
+        {
+            return _resumes.Values;
+        }
+
+        public Resume Find(string key)
+        {
+            Resume resume;
+            _resumes.TryGetValue(key, out resume);
+            return resume;
+        }
+
+        public Resume Remove(string key)
+        {
+            Resume resume;
+            _resumes.TryRemove(key, out resume);
+            return resume;
+        }
+
+        public void Update(Resume resume)
+        {
+            _resumes[resume.Key] = resume;
+        }
+    }
+}
