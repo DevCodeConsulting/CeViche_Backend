b1c2401ba0ba46b5e042446406dfe899b1a8293f
Gustaf Liljegren <gustafl@bahnhof.se>
2017-02-04 09:17:23 +0100

    First draft of model classes

---
+++ src/CeViche_Backend/Models/Employers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CeViche_Backend.Models
+{
+    public class Employers
+    {
+        public string Name { get; set; }
+        public List<string> Branch { get; set; }
+        public List<string> Technique { get; set; }
+    }
+}
